b921695bab1ebae8f6ec0cef3f6bcaaf598c5ddf
Daniel Schmocker <daniel.b.schmocker@gmail.com>
2023-09-16 10:41:44 +0200

    Samstag alles läuft

---
+++ DCMotorDriver_nRF24_LineFollower_PID/DCMotorDriver_nRF24_LineFollower_Kalman/DCMotorDriver_nRF24_LineFollower_Kalman.ino
@@ -0,0 +1,142 @@
+#include "RF24.h"
+#include "MeOrion.h"
+#include "SPI.h"
+#include "SimpleKalmanFilter.h"
+
+MeDCMotor motor3(M1);
+
+MeDCMotor motor4(M2);
+
+MeBuzzer buzzer;
+
+#define CE_PIN  10
+#define CSN_PIN  9
+
+#define LineFollower1_PIN  A2
+#define LineFollower2_PIN  A3
+
+#define GameOver_PIN A0
+
+
+//nRF24
+RF24 radio(CE_PIN, CSN_PIN); //CE, CSN
+
+
+
+int car = 1;//BLAU : 0, GRÜN 1, ROT 2, VIOLETT: 3};
+const byte address[][6] = {"Car10", "Car20", "Car30", "Car40"};
+
+
+int motorSpeed = 255;
+
+//Simple Klaman Filter
+float e_mean = 4; //fast: 2,2,1   intermediat: 3,2,1; slow; 4,2,1
+float e_est = 2;
+float q = 1;
+
+SimpleKalmanFilter kf_x = SimpleKalmanFilter(e_mean, e_est, q);// 4,4,1 init: 2,2,0.01
+SimpleKalmanFilter kf_y = SimpleKalmanFilter(e_mean, e_est, q);
+
+struct JoyStickData{
+  uint16_t x;
+  uint16_t y;
+  bool b;
+  String msg;
+};
+
+JoyStickData joyStickData;
+
+void setup(){
+
+  buzzer.setpin(8);
+  
+  Serial.begin(115200);
+  delay(1000);
+  Serial.println();
+  Serial.println("Serial started.");
+  Serial.println();
+  //nRF24
+  
+  if (!radio.begin()){
+    Serial.println(F("radio hardware not responding!!!"));
+    while(1){}
+  } else {
+    Serial.print(F("Radio Hardware ok.\n"));
+  }
+  radio.openReadingPipe(1,address[car]);
+  radio.setPALevel(RF24_PA_LOW);
+  radio.startListening();
+  //Serial.print(addressReading, BIN);
+  
+  //Linefollower
+  pinMode(LineFollower1_PIN, INPUT);
+  pinMode(LineFollower2_PIN, INPUT);
+
+  //GameOver
+  pinMode(GameOver_PIN, INPUT_PULLUP);
+
+  buzzer.tone(440, 100);
+
+}
+
+boolean vehicleInBlackField(){
+  int S1 = digitalRead(LineFollower1_PIN);
+  int S2  = digitalRead(LineFollower2_PIN);
+  return !((S1 == 1) || (S2 == 1));
+}
+
+float mapCoordinate(int coord){
+  float x = coord-255.;
+  float exponent = 2;
+  if (x >= 0){
+    return pow( sin(x/ 255. * PI / 2.), exponent) ;
+  } else {
+    return pow( sin(x/ 255. * PI / 2.), exponent) *-1;
+  }
+  
+}
+
+float rotateX(float x, float y, float angle){
+  return x * cos(angle) + y * sin(angle);
+}
+
+float rotateY(float x, float y, float angle){
+  return -x * sin(angle) + y * cos(angle);
+}
+
+void loop(){
+  boolean BallonGanz = digitalRead(GameOver_PIN);
+  
+  if(BallonGanz){
+  
+    if (radio.available()){
+      motorSpeed = 255;
+      radio.read(&joyStickData, sizeof(joyStickData));
+      float x = kf_x.updateEstimate(joyStickData.x);
+      float y = kf_y.updateEstimate(joyStickData.y);
+
+      float x_est = mapCoordinate(x);
+      float y_est = mapCoordinate(y);
+      float x_rot = rotateX(x_est, y_est, PI/4);
+      float y_rot = rotateY(x_est, y_est, PI/4);
+
+
+      float cutoff = 0.2;
+      
+      if (x_rot >= cutoff || y_rot >= cutoff || x_rot < -cutoff || y_rot < -cutoff){
+        float freq = 440*pow(2, (-y_rot*12+24*x_rot)/12);
+        Serial.println(freq);
+        buzzer.tone(freq, 50);
+        motor3.run(x_rot * motorSpeed);
+        motor4.run(y_rot * motorSpeed);
+      } else {
+        motor3.run(0);
+        motor4.run(0);
+      }
+
+    }
+    
+  } else {
+
+  }
+}
--- ESP32_nRF24/ESP32_nRF24.ino
+++ ESP32_nRF24/ESP32_nRF24.ino
@@ -1,23 +1,36 @@
 
 #include <RF24.h>
 #include <driver/adc.h>
-#include <WiFi.h>
+#include <Ticker.h>
 
 #include "OSCCommunication.h"
 
 RF24 radio(4, 5); // CE, CSN
-const byte address[6] = "Car30";
+
+
+
+int car = 1;//BLAU : 0, GRÜN 1, ROT 2, VIOLETT: 3};
+const byte address[][6] = {"Car10", "Car20", "Car30", "Car40"};
+
+
+
+struct JoyStickData {
+  uint16_t x; // X-axis value of joystick
+  uint16_t y; // Y-axis value of joystick
+  bool b; // Button state of joystick
+  String msg; // Message to be sent
+};
+
+
+
+JoyStickData joyStick_data; // Joystick data object
 
 OscCommunication Osc;
 
-struct JoyStickData {
-  uint16_t x;
-  uint16_t y;
-  bool b;
-  String msg;
-};
+Ticker timer;
 
 void setup() {
+
   Serial.begin(115200);
   delay(1000);
   Serial.println("Serial begin!");
@@ -31,50 +44,41 @@ void setup() {
       // Wait indefinitely if radio hardware is not responding
     }
   }
-  radio.openWritingPipe(address);
+  radio.openWritingPipe(address[car]);
   radio.setPALevel(RF24_PA_MIN);
   radio.stopListening();
 
   adc1_config_width(ADC_WIDTH_BIT_9);
+
+  // Task manager
+  timer.attach_ms(100, sendJoyStickData);
 }
 
-JoyStickData joyStick_data;
+void sendJoyStickData(){
+  Serial.print("Sending data.");
+  Serial.print("\t");
+  Serial.println(joyStick_data.x);
+  bool report = radio.write(&joyStick_data, sizeof(joyStick_data)); 
+  if (report) {
+    Serial.print(F("Transmission successful!"));
+  } else {
+    Serial.print(F("Transmission failed!"));
+  }
+  //radio.startListening();
+}
 
 void loop() {
   Osc.Update();
   if (Osc.gameState) {
     joyStick_data.x = adc1_get_raw(ADC1_CHANNEL_6);
     joyStick_data.y = adc1_get_raw(ADC1_CHANNEL_7);
-
-    bool report = radio.write(&joyStick_data, sizeof(joyStick_data));
-
-    if (report) {
-      Serial.print(F("Transmission successful!"));
-
-    } else {
-      Serial.print(F("Transmission failed!"));
-    }
-
-    Serial.println();
-    Serial.printf("x: %d y: %d\n", joyStick_data.x, joyStick_data.y);
-
-    delay(100);
+    //Serial.println();
+    //Serial.printf("x: %d y: %d\n", joyStick_data.x, joyStick_data.y);
   } else {
     joyStick_data.x = (uint16_t) 255;
     joyStick_data.y = (uint16_t) 255;
 
-    bool report = radio.write(&joyStick_data, sizeof(joyStick_data));
-
-    if (report) {
-      Serial.print(F("Transmission successful!"));
-
-    } else {
-      Serial.print(F("Transmission failed!"));
-    }
-
-    Serial.println();
-    Serial.println("Car off.");
-
-    delay(100);
+    //Serial.println();
+    //Serial.println("Car off.");
   }
 }
